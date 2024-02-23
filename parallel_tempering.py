7e5b07e17037593b7d7fd4279efdc8b6ab1deb37
adlermaus <117273465+adlermaus@users.noreply.github.com>
2024-02-23 12:33:03 +0100

    Update parallel_tempering.py

--- parallel_tempering.py
+++ parallel_tempering.py
@@ -83,6 +83,7 @@ def heat_capacity(energies, T, kB=1):
 def magnetic_susceptibility(magnetizations, T, kB=1):
     return 1/(kB * T) * np.var(magnetizations)
 
+
 def plot_E_M_a(Es, Ms, angles, q, J, T):
     fig,axs = plt.subplots(3, 1, constrained_layout=True, dpi=150)
     axs[0].plot(Es, '.',markersize=0.2)
@@ -99,15 +100,60 @@ def plot_E_M_a(Es, Ms, angles, q, J, T):
     axs[2].set_ylabel('Direction')
     fig.suptitle("q = %g, J = %g, T = %g" %(q, J, T))
 
-def phase_transitions(shape, q, J, Ts, sweeps):
+def phase_transitions(shape, q, J, Ts, sweeps, data_start=800000, kB=1):
     cVs = []
     Xis = []
     for T in Ts:
-        field, Es, Ms, angles = metropolis(shape, q, J, T, sweeps)
+        field, Es, Ms, angles = metropolis(shape, q, J, T, sweeps, kB)
         plot_E_M_a(Es, Ms, angles, q, J, T)
-        cVs.append(heat_capacity(Es[400000:], T))
-        Xis.append(magnetic_susceptibility(Ms[400000:], T))
+        cVs.append(heat_capacity(Es[data_start:], T, kB))
+        Xis.append(magnetic_susceptibility(Ms[data_start:], T, kB))
         print("ah")
+    fig,axs = plt.subplots(2, 1, constrained_layout=True)
+    axs[0].plot(Ts, cVs, '.--')
+    axs[0].set_xlabel('T')
+    axs[0].set_ylabel('c_V')
+    axs[1].plot(Ts, Xis, '.--')
+    axs[1].set_xlabel('T')
+    axs[1].set_ylabel('Xi')
+    plt.title("q = %g, J = %g" %(q, J))
+    plt.show()
+
+def parallel_tempering(shape, q, J, Ts, sweeps, kB=1):
+    replicas = [initialize_lattice(shape, q) for _ in range(len(Ts))]
+    Es_T = []
+    Ms_T = []
+    angles_T = []
+    for i, T in enumerate(Ts):
+        Es_T.append([energy(replicas[i], J)])
+        M_T, angle_T = magnetization(replicas[i], q)
+        Ms_T.append([M_T])
+        angles_T.append([angle_T])
+    for step in range(sweeps):
+        print(100/sweeps * step, " %")
+        for i, T in enumerate(Ts):
+            replicas[i], delta_E = metropolis_local(replicas[i], J, T)
+            Es_T[i].append(Es_T[i][-1] + delta_E)
+            M_T, angle_T = magnetization(replicas[i], q)
+            Ms_T[i].append(M_T)
+            angles_T[i].append(angle_T)
+        for i in range(len(Ts) - 1):
+            beta1, beta2 = 1/(kB*Ts[i]), 1/(Ts[i+1]*kB)
+            exponent = (beta1 - beta2) * (Es_T[i][-1] - Es_T[i + 1][-1])
+            if np.random.rand() < np.exp(-exponent):
+                replicas[i], replicas[i + 1] = replicas[i + 1], replicas[i]
+                Es_T[i][-1], Es_T[i+1][-1] = Es_T[i+1][-1], Es_T[i][-1]
+                Ms_T[i][-1], Ms_T[i+1][-1] = Ms_T[i+1][-1], Ms_T[i][-1]
+                angles_T[i][-1], angles_T[i+1][-1] = angles_T[i+1][-1], angles_T[i][-1]
+    return replicas, Es_T, Ms_T, angles_T
+
+def plots_parallel_tempering(q, J, Ts, Es_T, Ms_T, angles_T, data_start):
+    cVs = []
+    Xis = []
+    for E_T, M_T, angle_T, T in zip(Es_T, Ms_T, angles_T, Ts):
+        plot_E_M_a(E_T, M_T, angle_T, q, J, T)
+        cVs.append(heat_capacity(E_T[data_start:], T))
+        Xis.append(magnetic_susceptibility(M_T[data_start:], T))
     fig,axs = plt.subplots(2, 1, constrained_layout=True)
     axs[0].plot(Ts, cVs, '.--')
     axs[0].set_xlabel('T')
@@ -120,29 +166,43 @@ def phase_transitions(shape, q, J, Ts, sweeps):
 
 
 shape = (20, 20)
-q = 2
+q = 5
 J = 1
 T = 1.135
-Ts = [0.9, 0.95, 1, 1.05, 1.1, 1.135, 1.15, 1.2, 1.25, 1.3, 1.35]
-sweeps = 1000000
+Ts = [0.7, 0.75, 0.8, 0.825, 0.85, 0.875, 0.9, 0.95, 1]
+sweeps = 500000
+data_start = 250000
 
+
+# parallel tempering
 t0 = time.time()
-phase_transitions(shape, q, J, Ts, sweeps)
+fields, Es_T, Ms_T, angles_T = parallel_tempering(shape, q, J, Ts, sweeps)
 t1 = time.time()
 print((t1-t0)/60)
 
+plots_parallel_tempering(q, J, Ts, Es_T, Ms_T, angles_T, data_start)
 
+
+
+## metropolis for phase transition
+# t0 = time.time()
+# phase_transitions(shape, q, J, Ts, sweeps)
+# t1 = time.time()
+# print((t1-t0)/60)
+
+
+
+## metropolis
 # t0 = time.time()
 # field, Es, Ms, angles = metropolis(shape, q, J, T, sweeps)
 # t1 = time.time()
 # print((t1-t0)/60, "min")
 
-
 # show_config(field, q)
 # plot_E_M_a(Es, Ms, angles, q, J, T)
 
-# cV = heat_capacity(Es[800000:], T)
-# Xi = magnetic_susceptibility(Ms[800000:], T)
+# cV = heat_capacity(Es[data_start:], T)
+# Xi = magnetic_susceptibility(Ms[data_start:], T)
 
 # print(cV)
 # print(Xi)
